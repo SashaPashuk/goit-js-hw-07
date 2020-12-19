5ca3735cc99118b318437129fbd232e88fe89c59
Alexander <pashuk.sasha18@gmail.com>
2020-12-19 11:23:27 +0200

    initial commit

---
+++ js/task-7.js
@@ -0,0 +1,5 @@
+let controlFont = document.getElementById("font-size-control");
+let text = document.getElementById("text");
+controlFont.oninput = function() {
+  text.style.fontSize = controlFont.value + "px";
+};
